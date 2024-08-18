08db3ca4c51caa5e7902c1bda1dc88e61dd902c2
Pavat <pavatnon@gmail.com>
2024-08-18 19:04:30 +0700

    update
    แปะcontainer ให้ให้ครบเฉยๆ จะได้ไม่สับสน leyout

--- Restaurant_System/backoffice-site/src/views/CheckoutPage/CheckoutView.vue
+++ Restaurant_System/backoffice-site/src/views/CheckoutPage/CheckoutView.vue
@@ -1,6 +1,8 @@
 <template>
     <Leyout>
-        checkoutpage
+        <div class="container mx-auto p-10">
+            <div class="text-center text-2xl font-bold">รายการCheckout</div>
+        </div>
 
     </Leyout>
 </template>
